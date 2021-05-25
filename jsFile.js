a36b4d15872d92a46ed2b776e865277c1199f568
sonukiwi <sonukiwi17@gmail.com>
2021-05-25 16:19:29 +0530

    3 Added Files

---
+++ jsFile.js
@@ -0,0 +1,26 @@
+
+$(document).ready(
+    function()
+    {
+        $("#button1").click(function()
+        {
+           $("#dip_comp_sci").slideToggle();
+        }); 
+        $("#button2").click(function()
+        {
+            $("#free_eng_aca").slideToggle();
+        });
+        $("#button3").click(function()
+        {
+            $("#soe").slideToggle();
+        });
+        $("#button4").click(function()
+        {
+            $("#tutoraj").slideToggle();
+        });
+        $("#button5").click(function()
+        {
+            $("#abhayshala").slideToggle();
+        });
+    }
+);
